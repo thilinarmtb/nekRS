c5c7c4ae1aaf3c4e6574523bacab99dd48f05868
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-04-09 14:24:04 -0400

    Squashed '3rd_party/gemv/' changes from 6adfda0a..18dfca47
    18dfca47 Finish the cuda backend implementation
    
    git-subtree-dir: 3rd_party/gemv
    git-subtree-split: 18dfca470a459bfaf176c59f613d8ef67613ae76

--- backends/gemv-backend-cuda.c
+++ backends/gemv-backend-cuda.c
@@ -15,10 +15,35 @@ static inline void check_cuda_rtc_(nvrtcResult status, const char *file,
 static void *d_A = NULL;
 static int initialized = 0;
 
-static void cuda_gemv(void *d_y, const void *d_x, const struct gemv_t *gemv) {}
+static CUmodule module = NULL;
+static CUfunction kernel = NULL;
+
+static int block_size = 32;
+static int kernel_id = 1;
+
+#include "gemv-backend-unified-cuda-hip-kernels.h"
+
+static void cuda_run(void *d_y, const void *d_x, const struct gemv_t *gemv) {
+  if (!initialized)
+    gemv_log(GEMV_ERROR, "cuda_run: CUDA backend is not initialized !");
+
+  gemv_log(GEMV_INFO, "y = %p, x = %p, m = %u, n = %u", d_y, d_x, gemv->m,
+           gemv->n);
+
+  void *arguments[] = {&d_y, &d_A, &d_x, (void *)&gemv->m, (void *)&gemv->n};
+  check_cuda_runtime(cuLaunchKernel(kernel, (gemv->m + 31) / 32, 1, 1, 32, 1, 1,
+                                    0, 0, arguments, NULL));
+}
 
 static void cuda_finalize(void) {
+  gemv_log(GEMV_INFO, "cuda_finalize: ...");
+  if (!initialized) return;
+
   check_cuda_runtime(cudaFree(d_A)), d_A = NULL;
+  check_cuda_runtime(cuModuleUnload(module)), module = NULL, kernel = NULL;
+  initialized = 0;
+
+  gemv_log(GEMV_INFO, "cuda_finalize: done.");
 }
 
 static void cuda_init_aux(const struct gemv_t *gemv) {
@@ -35,8 +60,50 @@ static void cuda_init_aux(const struct gemv_t *gemv) {
       cudaMemcpy(d_A, A, m * n * unit_size, cudaMemcpyHostToDevice));
   gemv_free(&A);
 
-  // char source[BUFSIZ];
-  // const char *precision = gemv_precision_to_str(gemv->precision);
+  char source[BUFSIZ];
+  const char *precision = gemv_precision_to_str(gemv->precision);
+
+  switch (kernel_id) {
+  case 0:
+    snprintf(source, BUFSIZ, gemv_kernels[0], precision, precision, precision,
+             precision);
+    break;
+  case 1:
+    snprintf(source, BUFSIZ, gemv_kernels[1], block_size, precision, precision,
+             precision, precision);
+    break;
+  default:
+    gemv_log(GEMV_ERROR, "cuda_init_aux: invalid kernel id = %d", kernel_id);
+    break;
+  }
+  gemv_log(GEMV_INFO, "cuda_init_aux: kernel id = %d, source = \n%s", kernel_id,
+           source);
+
+  nvrtcProgram program = NULL;
+  check_cuda_rtc(nvrtcCreateProgram(&program, source, NULL, 0, NULL, NULL));
+  nvrtcResult status = nvrtcCompileProgram(program, 0, NULL);
+  if (status == NVRTC_SUCCESS) goto generate_kernel;
+
+  size_t log_size;
+  check_cuda_rtc(nvrtcGetProgramLogSize(program, &log_size));
+  char *log = gemv_calloc(char, log_size + 1);
+  check_cuda_rtc(nvrtcGetProgramLog(program, log));
+  fprintf(stderr, "[ERROR] cuda_init_aux: kernel compilation error:\n%s\n",
+          log);
+  fflush(stderr);
+  gemv_free(&log);
+  gemv_log(GEMV_ERROR, "cuda_init_aux: kernel compilation failed !");
+
+  size_t size;
+generate_kernel:
+  check_cuda_rtc(nvrtcGetPTXSize(program, &size));
+  char *binary_data = gemv_calloc(char, size + 1);
+  check_cuda_rtc(nvrtcGetPTX(program, binary_data));
+  check_cuda_rtc(nvrtcDestroyProgram(&program));
+
+  check_cuda_runtime(cuModuleLoadData(&module, binary_data));
+  gemv_free(&binary_data);
+  check_cuda_runtime(cuModuleGetFunction(&kernel, module, "gemv"));
 }
 
 static void cuda_init(struct gemv_backend_t *backend,
@@ -44,12 +111,16 @@ static void cuda_init(struct gemv_backend_t *backend,
   gemv_log(GEMV_INFO, "cuda_init: ...");
   if (initialized) return;
 
-  // TODO: Set backend functions.
+  backend->malloc = cuda_malloc;
+  backend->free = cuda_free;
+  backend->copy = cuda_copy;
+  backend->run = cuda_run;
+  backend->finalize = cuda_finalize;
 
   cuda_init_aux(gemv);
 
   initialized = 1;
-  gemv_log(GEMV_INFO, "hip_init: done.");
+  gemv_log(GEMV_INFO, "cuda_init: done.");
 }
 
 void gemv_register_cuda(void) { gemv_backend_register("cuda", cuda_init); }
