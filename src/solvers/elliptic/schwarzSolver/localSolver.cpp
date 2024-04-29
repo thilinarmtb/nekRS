d833f31634a63ff7fbd1092437b5a0ec05531b66
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-04-29 13:36:48 -0400

    Only call local solver if it is setup

--- src/solvers/elliptic/schwarzSolver/localSolver.cpp
+++ src/solvers/elliptic/schwarzSolver/localSolver.cpp
@@ -164,7 +164,7 @@ void LocalSolver_t<val_t>::Solve(Vec_t &x_, const Vec_t &rhs_) {
   for (unsigned i = 0; i < input_size; i++)
     if (u_to_c[i] >= 0) rhs[u_to_c[i]] += rhs_[i];
 
-  solver->Solve(x, rhs);
+  if (solver != nullptr) solver->Solve(x, rhs);
 
   for (unsigned i = 0; i < input_size; i++) {
     if (u_to_c[i] >= 0)
